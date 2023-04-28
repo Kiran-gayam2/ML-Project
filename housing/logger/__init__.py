7d29fa077a609989b601a43ffd0e76e0843230c8
kiran <gayamvenkatakiran@gmail.com>
2023-04-28 16:48:35 +0200

    'exception_logging_folders'

--- housing/logger/__init__.py
+++ housing/logger/__init__.py
@@ -1,17 +1,15 @@
 import logging
+import os
+from datetime import datetime
 
 LOG_DIR = "housing_log"
 
-CURRENT_TIME_STEP = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%s')}"
+CURRENT_TIME_STEP = f"{(datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))}"
 
 LOG_FILE_NAME = f"log_{CURRENT_TIME_STEP}.log"
 
-os.mkdirs(LOG_DIR)
+os.makedirs(LOG_DIR,exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(LOG_DIR,LOG_FILE_NAME)
 
-logging.basicConfig(filename=LOG_FILE_PATH,
-                    filemode='w',
-                    format='[%(asctime)s] %(name)s - %(levelname)s - %(message)s',
-                    level = logging.INFO
-                    )
+logging.basicConfig(filename=LOG_FILE_PATH,filemode='w',format='[%(asctime)s] %(name)s - %(levelname)s - %(message)s',level = logging.INFO)
